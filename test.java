98a73bcd1a77d8786c84428a283824fadaa18421
mdaydevelopment <day.michael.b@gmail.com>
2019-02-27 15:10:32 -0600

    test commit of test class to test repo

--- test.java
+++ test.java
@@ -1,3 +1,8 @@
 import java.util.*;
 
-// I am addingmore 
+// I am addingmore
+public class test {
+    public static void main(String[] args) {
+        System.out.println("This is a test of the testing repo's test class with a test string for testing.");
+    }
+}
