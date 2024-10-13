a326ea45b83e966d015110c120e339e2e4b398b5
bobjackson-code <156550968+bobjackson-code@users.noreply.github.com>
2024-10-12 20:05:12 -0500

    Update helloWorld.java

--- Hello World/src/helloWorld.java
+++ Hello World/src/helloWorld.java
@@ -2,6 +2,6 @@
 public class helloWorld{
 	//Justin Ridout
 	public static void main(String[] args) {
-		System.out.println("Hello Word");
+		System.out.println("Hello World - CIS152 Data Structures");
 	}
 }
