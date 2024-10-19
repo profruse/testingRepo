5a5da1eb5f3e509224dad749f0e40d298a616507
Your Name <lchulu@dmacc.edu>
2024-10-18 19:32:04 -0500

    Updated code

--- Hello World/src/helloWorld.java
+++ Hello World/src/helloWorld.java
@@ -2,6 +2,6 @@
 public class helloWorld{
 	//Justin Ridout
 	public static void main(String[] args) {
-		System.out.println("Hello Word");
+		System.out.println("Hello Class 152, hope everyone have a great weekend!");
 	}
 }
