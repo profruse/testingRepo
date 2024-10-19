17650be978a005523e1238d12a83b8125592ae57
Your Name <lchulu@dmacc.edu>
2024-10-18 19:33:08 -0500

    updated to my name LC

--- Hello World/src/helloWorld.java
+++ Hello World/src/helloWorld.java
@@ -1,6 +1,6 @@
 
 public class helloWorld{
-	//Justin Ridout
+	//Louise Chulu
 	public static void main(String[] args) {
 		System.out.println("Hello Class 152, hope everyone have a great weekend!");
 	}
